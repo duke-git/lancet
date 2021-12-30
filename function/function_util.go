5e5ae92f9c043a132d18ff19721435d3050a62db
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2021-12-30 11:41:34 +0100

    function: catch earlier programming error
    Place at first line of the function body a function type safe guard
    check.

--- function/function_util.go
+++ function/function_util.go
@@ -14,10 +14,26 @@ func invokeFunc(fn interface{}, args ...interface{}) []reflect.Value {
 	return fv.Call(params)
 }
 
+func unsafeInvokeFunc(fn interface{}, args ...interface{}) []reflect.Value {
+	fv := reflect.ValueOf(fn)
+	params := make([]reflect.Value, len(args))
+	for i, item := range args {
+		params[i] = reflect.ValueOf(item)
+	}
+	return fv.Call(params)
+}
+
 func functionValue(function interface{}) reflect.Value {
 	v := reflect.ValueOf(function)
 	if v.Kind() != reflect.Func {
 		panic(fmt.Sprintf("Invalid function type, value of type %T", function))
 	}
 	return v
+}
+
+func MustBeFunction(function interface{}) {
+	v := reflect.ValueOf(function)
+	if v.Kind() != reflect.Func {
+		panic(fmt.Sprintf("Invalid function type, value of type %T", function))
+	}
 }
