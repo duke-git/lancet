b0e17c7bc461826ce5ba1a3ed91b425296d10059
dudaodong <dudaodong@gmail.com>
2022-01-09 15:39:50 +0800

    refactor: rewrite all unit test functions with assert

--- formatter/formatter_test.go
+++ formatter/formatter_test.go
@@ -7,22 +7,17 @@ import (
 )
 
 func TestComma(t *testing.T) {
-	comma(t, "", "", "")
-	comma(t, "aa", "", "")
-	comma(t, "aa.a", "", "")
-	comma(t, []int{1}, "", "")
-	comma(t, "123", "", "123")
-	comma(t, "12345", "", "12,345")
-	comma(t, 12345, "", "12,345")
-	comma(t, 12345, "$", "$12,345")
-	comma(t, 12345, "¥", "¥12,345")
-	comma(t, 12345.6789, "", "12,345.6789")
-}
+	assert := internal.NewAssert(t, "TestComma")
 
-func comma(t *testing.T, test interface{}, symbol string, expected interface{}) {
-	res := Comma(test, symbol)
-	if res != expected {
-		internal.LogFailedTestInfo(t, "Comma", test, expected, res)
-		t.FailNow()
-	}
+	assert.Equal("", Comma("", ""))
+	assert.Equal("", Comma("aa", ""))
+	assert.Equal("", Comma("aa.a", ""))
+	assert.Equal("", Comma([]int{1}, ""))
+	assert.Equal("123", Comma("123", ""))
+	assert.Equal("12,345", Comma("12345", ""))
+
+	assert.Equal("12,345", Comma(12345, ""))
+	assert.Equal("$12,345", Comma(12345, "$"))
+	assert.Equal("¥12,345", Comma(12345, "¥"))
+	assert.Equal("12,345.6789", Comma(12345.6789, ""))
 }
