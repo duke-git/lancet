eb59d02a0898fe67721c4babf6f2424192abede7
dudaodong <lanliddd.2007@163.com>
2022-02-04 19:35:06 +0800

    feat: add stackarray.go implements stack with array

--- datastructure/node.go
+++ datastructure/node.go
@@ -1,13 +1,24 @@
 package datastructure
 
-// LinkNode is a linkedlist node, which have a value and Pre points to previous node, Next points to a next node of the link.
+// LinkNode is a linkedlist node, which have a Value and Pre points to previous node, Next points to a next node of the link.
 type LinkNode[T any] struct {
 	Value T
 	Pre   *LinkNode[T]
 	Next  *LinkNode[T]
 }
 
+// NewLinkNode return a LinkNode pointer
 func NewLinkNode[T any](value T) *LinkNode[T] {
 	return &LinkNode[T]{value, nil, nil}
 }
 
+// StackNode is a node in stack, which have a Value and Pre points to previous node in the stack.
+type StackNode[T any] struct {
+	Value T
+	Pre   *StackNode[T]
+}
+
+// NewStackNode return a StackNode pointer
+func NewStackNode[T any](value T) *StackNode[T] {
+	return &StackNode[T]{value, nil}
+}
