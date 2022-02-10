797e47363f2f7322b19505a4fa68b1e9832a03f1
dudaodong <lanliddd.2007@163.com>
2022-02-10 11:50:03 +0800

    rename: rename StackArray -> ArrayStack, StackLink -> LinkedStack

---
+++ datastructure/linkedstack.go
@@ -0,0 +1,92 @@
+package datastructure
+
+import (
+	"errors"
+	"fmt"
+)
+
+// LinkedStack implements stack with link list
+type LinkedStack[T any] struct {
+	top    *StackNode[T]
+	length int
+}
+
+// NewLinkedStack return a empty LinkedStack pointer
+func NewLinkedStack[T any]() *LinkedStack[T] {
+	return &LinkedStack[T]{top: nil, length: 0}
+}
+
+// Data return stack data
+func (s *LinkedStack[T]) Data() []T {
+	res := []T{}
+	current := s.top
+
+	for current != nil {
+		res = append(res, current.Value)
+		current = current.Next
+	}
+	return res
+}
+
+// Length return length of stack data
+func (s *LinkedStack[T]) Length() int {
+	return s.length
+}
+
+// IsEmpty checks if stack is empty or not
+func (s *LinkedStack[T]) IsEmpty() bool {
+	return s.length == 0
+}
+
+// Push element into stack
+func (s *LinkedStack[T]) Push(value T) {
+	newNode := NewStackNode(value)
+	top := s.top
+	if top == nil {
+		s.top = newNode
+	} else {
+		newNode.Next = top
+		s.top = newNode
+	}
+
+	s.length++
+}
+
+// Pop delete the top element of stack then return it, if stack is empty, return nil and error
+func (s *LinkedStack[T]) Pop() (*T, error) {
+	if s.IsEmpty() {
+		return nil, errors.New("stack is empty")
+	}
+
+	top := s.top
+	s.top = s.top.Next
+	s.length--
+
+	return &top.Value, nil
+}
+
+// Peak return the top element of stack then return it
+func (s *LinkedStack[T]) Peak() (*T, error) {
+	if s.IsEmpty() {
+		return nil, errors.New("stack is empty")
+	}
+	return &s.top.Value, nil
+}
+
+// EmptyStack clear the stack data
+func (s *LinkedStack[T]) EmptyStack() {
+	s.top = nil
+	s.length = 0
+}
+
+// Print all nodes info of stack link
+func (s *LinkedStack[T]) Print() {
+	current := s.top
+	info := "[ "
+	for current != nil {
+		info += fmt.Sprintf("%+v, ", current)
+		current = current.Next
+	}
+	info += " ]"
+	fmt.Println(info)
+}
---
+++ datastructure/linkedstack_test.go
@@ -0,0 +1,80 @@
+package datastructure
+
+import (
+	"testing"
+
+	"github.com/duke-git/lancet/internal"
+)
+
+func TestLinkedStack_Push(t *testing.T) {
+	assert := internal.NewAssert(t, "TestLinkedStack_Push")
+
+	stack := NewLinkedStack[int]()
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+
+	stack.Print()
+
+	expected := []int{3, 2, 1}
+	values := stack.Data()
+	length := stack.Length()
+
+	assert.Equal(expected, values)
+	assert.Equal(3, length)
+}
+
+func TestLinkedStack_Pop(t *testing.T) {
+	assert := internal.NewAssert(t, "TestLinkedStack_Pop")
+
+	stack := NewLinkedStack[int]()
+	topItem, err := stack.Pop()
+	assert.IsNotNil(err)
+
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+
+	topItem, err = stack.Pop()
+	assert.IsNil(err)
+	assert.Equal(3, *topItem)
+
+	expected := []int{2, 1}
+	stack.Print()
+	assert.Equal(expected, stack.Data())
+}
+
+func TestLinkedStack_Peak(t *testing.T) {
+	assert := internal.NewAssert(t, "TestLinkedStack_Peak")
+
+	stack := NewLinkedStack[int]()
+	topItem, err := stack.Peak()
+	assert.IsNotNil(err)
+
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+
+	topItem, err = stack.Peak()
+	assert.IsNil(err)
+	assert.Equal(3, *topItem)
+
+	expected := []int{3, 2, 1}
+	assert.Equal(expected, stack.Data())
+}
+
+func TestLinkedStack_Empty(t *testing.T) {
+	assert := internal.NewAssert(t, "TestLinkedStack_Empty")
+
+	stack := NewLinkedStack[int]()
+	assert.Equal(true, stack.IsEmpty())
+	assert.Equal(0, stack.Length())
+
+	stack.Push(1)
+	assert.Equal(false, stack.IsEmpty())
+	assert.Equal(1, stack.Length())
+
+	stack.EmptyStack()
+	assert.Equal(true, stack.IsEmpty())
+	assert.Equal(0, stack.Length())
+}
