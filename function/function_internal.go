b48155c2498c07b343404a29e8afa4fd88261eb5
dudaodong <lanliddd.2007@163.com>
2022-02-19 21:52:10 +0800

    refactor: rename files *_util.go to *_internal.go

---
+++ function/function_internal.go
@@ -0,0 +1,39 @@
+package function
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func invokeFunc(fn interface{}, args ...interface{}) []reflect.Value {
+	fv := functionValue(fn)
+	params := make([]reflect.Value, len(args))
+	for i, item := range args {
+		params[i] = reflect.ValueOf(item)
+	}
+	return fv.Call(params)
+}
+
+func unsafeInvokeFunc(fn interface{}, args ...interface{}) []reflect.Value {
+	fv := reflect.ValueOf(fn)
+	params := make([]reflect.Value, len(args))
+	for i, item := range args {
+		params[i] = reflect.ValueOf(item)
+	}
+	return fv.Call(params)
+}
+
+func functionValue(function interface{}) reflect.Value {
+	v := reflect.ValueOf(function)
+	if v.Kind() != reflect.Func {
+		panic(fmt.Sprintf("Invalid function type, value of type %T", function))
+	}
+	return v
+}
+
+func mustBeFunction(function interface{}) {
+	v := reflect.ValueOf(function)
+	if v.Kind() != reflect.Func {
+		panic(fmt.Sprintf("Invalid function type, value of type %T", function))
+	}
+}
