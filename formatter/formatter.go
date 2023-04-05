3685aee02bb10641b27ea8dd3e794ab5b447f2ab
dudaodong <dudaodong@gmail.com>
2023-04-05 14:26:42 +0800

    fix: fix bug of Comma function

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -5,6 +5,11 @@
 package formatter
 
 import (
+	"fmt"
+
+	"github.com/duke-git/lancet/v2/convertor"
+	"github.com/duke-git/lancet/v2/strutil"
+	"github.com/duke-git/lancet/v2/validator"
 	"golang.org/x/exp/constraints"
 )
 
@@ -13,57 +18,29 @@ import (
 // Comma("12345", "$") => "$12,345", Comma(12345, "$") => "$12,345"
 // Play: https://go.dev/play/p/eRD5k2vzUVX
 func Comma[T constraints.Float | constraints.Integer | string](value T, symbol string) string {
-	// s, err := numberToString(value)
-	// if err != nil {
-	// 	return ""
-	// }
+	if validator.IsInt(value) {
+		v, err := convertor.ToInt(value)
+		if err != nil {
+			return ""
+		}
+		return symbol + commaInt(v)
+	}
 
-	// dotIndex := strings.Index(s, ".")
-	// if dotIndex != -1 {
-	// 	return symbol + commaString(s[:dotIndex]) + s[dotIndex:]
-	// }
+	if validator.IsFloat(value) {
+		v, err := convertor.ToFloat(value)
+		if err != nil {
+			return ""
+		}
+		return symbol + commaFloat(v)
+	}
 
-	// return symbol + commaString(s)
+	if strutil.IsString(value) {
+		v := fmt.Sprintf("%v", value)
+		if validator.IsNumberStr(v) {
+			return symbol + commaStr(v)
+		}
+		return ""
+	}
 
 	return ""
-
 }
-
-// func commaString(s string) string {
-// 	if len(s) <= 3 {
-// 		return s
-// 	}
-// 	return commaString(s[:len(s)-3]) + "," + commaString(s[len(s)-3:])
-// }
-
-// func numberToString(value any) (string, error) {
-// 	switch reflect.TypeOf(value).Kind() {
-// 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
-// 		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-// 		return fmt.Sprintf("%v", value), nil
-
-// 		// todo: need to handle 12345678.9 => 1.23456789e+07
-// 	case reflect.Float32, reflect.Float64:
-// 		return fmt.Sprintf("%v", value), nil
-
-// 	case reflect.String:
-// 		{
-// 			sv := fmt.Sprintf("%v", value)
-// 			if strings.Contains(sv, ".") {
-// 				_, err := strconv.ParseFloat(sv, 64)
-// 				if err != nil {
-// 					return "", err
-// 				}
-// 				return sv, nil
-// 			} else {
-// 				_, err := strconv.ParseInt(sv, 10, 64)
-// 				if err != nil {
-// 					return "", nil
-// 				}
-// 				return sv, nil
-// 			}
-// 		}
-// 	default:
-// 		return "", nil
-// 	}
-// }
