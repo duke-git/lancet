1d94896c9bba8380ad5c9dc21167be4c6e2bc5ad
Kyden <kytedance@gmail.com>
2024-09-15 20:29:12 +0800

    fix(strutil): rename PadStart to Pad. (#245)

--- strutil/string.go
+++ strutil/string.go
@@ -75,7 +75,7 @@ func LowerFirst(s string) string {
 	return string(r) + s[size:]
 }
 
-// PadStart pads string on the left and right side if it's shorter than size.
+// Pad pads string on the left and right side if it's shorter than size.
 // Padding characters are truncated if they exceed size.
 // Play: https://go.dev/play/p/NzImQq-VF8q
 func Pad(source string, size int, padStr string) string {
