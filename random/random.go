a769257017fe31256674bf497e54be9fefc92441
dudaodong <lanliddd.2007@163.com>
2024-11-11 19:45:31 +0800

    fix: update random seed when call random function

--- random/random.go
+++ random/random.go
@@ -273,6 +273,9 @@ func nearestPowerOfTwo(cap int) int {
 
 // random generate a random string based on given string range.
 func random(s string, length int) string {
+	// 确保随机数生成器的种子是动态的
+	rand.Seed(time.Now().UnixNano())
+
 	// 仿照strings.Builder
 	// 创建一个长度为 length 的字节切片
 	bytes := make([]byte, length)
